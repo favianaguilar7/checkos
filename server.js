b20b4346960dd33c14742f3cebadf456ed0b4404
favianaguilar7 <favianaguilar7@gmail.com>
2024-08-20 13:56:40 -0600

    menu y funcion start

--- server.js
+++ server.js
@@ -114,7 +114,7 @@ Comentario: ${comment}
     });
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Servidor escuchando en el puerto ${PORT}`);
 });

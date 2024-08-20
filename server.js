eb751be9ab71eac145903ba5af605f92708f1189
favianaguilar7 <favianaguilar7@gmail.com>
2024-08-20 11:36:58 -0600

    dfghj

--- server.js
+++ server.js
@@ -114,7 +114,7 @@ Comentario: ${comment}
     });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Servidor escuchando en el puerto ${PORT}`);
 });
